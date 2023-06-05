67f32ca8adfc297000ead20ec678358bde0ee363
Simon Lee <simon.lee@cardinalblue.com>
2023-06-05 16:17:48 +0800

    feat: integrate supabase to store summaries

--- pages/api/summary.ts
+++ pages/api/summary.ts
@@ -3,6 +3,7 @@ import { WWDCTranscriptLoader } from "@/utils/wwdcTranscriptLoader";
 import { makeSummaryChain } from '@/utils/makechain';
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import NextCors from 'nextjs-cors';
+import { supabase } from '@/utils/supabase';
 
 export default async function handler(
   req: NextApiRequest,
@@ -30,21 +31,68 @@ export default async function handler(
 
   console.log('sessionId', sessionId);
   try {
-    const loader = new WWDCTranscriptLoader({ sessionId: sessionId });
-    const documents = await loader.load();
-    const splitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 4096,
-      chunkOverlap: 400,
-    });
-    const splitDocs = await splitter.splitDocuments(documents);
-    const chain = await makeSummaryChain(sessionId)
-    const response = await chain.call({
-      input_documents: splitDocs,
-    });
+    const summary = await handleSummaryRequest(sessionId);
 
-    res.status(200).json(response);
+    res.status(200).json({ summary });
   } catch (error: any) {
     console.log('error', error);
     res.status(500).json({ error: error.message || 'Something went wrong' });
   }
 };
+
+async function handleSummaryRequest(sessionId: string): Promise<string> {
+  const summary = await getSummaryFromDb(sessionId);
+  if (summary) {
+    return summary;
+  }
+
+  const newSummary = await generateSummary(sessionId);
+  await writeSummaryToDb(sessionId, newSummary);
+  return newSummary;
+}
+
+async function generateSummary(sessionId: string) {
+  const loader = new WWDCTranscriptLoader({ sessionId: sessionId });
+  const documents = await loader.load();
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 4096,
+    chunkOverlap: 400,
+  });
+  const splitDocs = await splitter.splitDocuments(documents);
+  const chain = await makeSummaryChain(sessionId)
+  const response = await chain.call({
+    input_documents: splitDocs,
+  });
+
+  return response.text;
+}
+
+async function writeSummaryToDb(sessionId: string, summary: string) {
+  const { data, error } = await supabase
+    .from('session_summary')
+    .insert([{ session_id: sessionId, summary: summary }]);
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+}
+
+async function getSummaryFromDb(sessionId: string): Promise<any> {
+  const { data, error } = await supabase
+    .from('session_summary')
+    .select('summary')
+    .eq('session_id', sessionId)
+    .maybeSingle();
+
+  if (error) {
+    throw error;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return data.summary;
+}
